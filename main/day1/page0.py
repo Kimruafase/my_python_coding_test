330a7c7e255aaa56b8187285c6032ee3c4052a12
kimruafase <ppooppplus33@gmail.com>
2024-11-08 22:52:38 +0900

    2024-11-08 10:52 day1

---
+++ main/day1/page0.py
@@ -0,0 +1,67 @@
+# day1 > page0.py
+
+def summary():
+    A, B = map(int, input().split(" "))
+
+    if 0 >= A:
+        return
+    if B >= 10:
+        return
+
+    print(A + B)
+
+# summary()
+
+def subtract():
+    A, B = map(int, input().split(" "))
+
+    if 0 >= A:
+        return
+    if B >= 10:
+        return
+
+    print(A - B)
+
+# subtract()
+
+
+def multiple():
+    A, B = map(int, input().split(" "))
+
+    if 0 >= A:
+        return
+    if B >= 10:
+        return
+
+    print(A * B)
+
+# multiple()
+
+def divide():
+    A, B = map(int, input().split(" "))
+
+    if 0 >= A:
+        return
+    if B > 10:
+        return
+
+    print(A / B)
+
+# divide()
+
+def operation():
+    A, B = map(int, input().split(" "))
+
+    if 0 >= A:
+        return
+    if B >= 10:
+        return
+
+    print(A + B)
+    print(A - B)
+    print(A * B)
+    print(A // B)
+    print(A % B)
+
+# operation()
+
---
+++ main/day1/page1.py
@@ -0,0 +1,59 @@
+# day1 > page1.py
+
+def surprise_print():
+    id = input()
+    if len(id) > 50:
+        return
+    print(f"{id}??!")
+
+# surprise_print()
+
+def year_cal_budda():
+    year = int(input())
+    print(f'{year-543}')
+
+# year_cal_budda()
+
+def A_B_C():
+    A, B, C = map(int, input().split())
+
+    print((A + B) % C)
+    print(((A % C) + (B % C)) % C)
+    print((A * B) % C)
+    print(((A % C) * (B % C)) % C)
+
+# A_B_C()
+
+def self_multiple():
+    A = int(input())
+    B = input()
+    print(f'{A * int(B[2])}')
+    print(f'{A * int(B[1])}')
+    print(f'{A * int(B[0])}')
+    print(f'{A * int(B)}')
+
+# self_multiple()
+
+def three_add():
+    A, B, C = map(int, input().split())
+
+    print(A + B + C)
+
+# three_add()
+
+def cat():
+    print("\    /\\")
+    print(" )  ( ')")
+    print("(  /  )")
+    print(" \\(__)|")
+
+# cat()
+
+def dog():
+    print("|\\_/|")
+    print("|q p|   /}")
+    print("( 0 )\"\"\"\\")
+    print("|\"^\"`    |")
+    print("||_/=\\\\__|")
+
+dog()
